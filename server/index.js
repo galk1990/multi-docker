b54fb6c046884403cd025a33e16599ae046f1738
Gal Klein <gal.klein.1990@gmail.com>
2024-06-12 14:25:29 +0300

    modified

---
+++ server/index.js
@@ -0,0 +1,75 @@
+const { Pool } = require("pg");
+const keys = require("./keys");
+const TAG = "server/index.js";
+
+// Express app setup
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors"); //what is cors? it is a package that allows for cross-origin requests
+//what are cross-origin requests? they are requests that are made from one domain to another domain
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Postgres Client Setup
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
+  ssl:
+    process.env.NODE_ENV !== "production"
+      ? false
+      : { rejectUnauthorized: false },
+});
+
+pgClient.on("connect", (client) => {
+  const createValuesTable = () =>
+    client
+      .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+      .catch((err) => console.log(TAG, "createValuesTable", err));
+  createValuesTable();
+});
+
+//Redis client setup
+const redis = require("redis");
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+
+const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
+
+app.get("/values/all", async (req, res) => {
+  const values = await pgClient.query("SELECT * from values");
+  res.send(values.rows);
+});
+
+app.get("/values/current", async (req, res) => {
+  redisClient.hgetall("values", (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post("/values", async (req, res) => {
+  const index = req.body.index;
+  if (parseInt(index) > 40) {
+    return res.status(422).send("Index too high");
+  }
+  redisClient.hset("values", index, "Nothing yet!");
+  redisPublisher.publish("insert", index);
+  pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+  res.send({ working: true });
+});
+
+app.listen(5000, (err) => {
+  console.log(TAG, "Listening in server on port 5000");
+});
---
+++ worker/index.js
@@ -0,0 +1,25 @@
+const redis = require("redis");
+const keys = require("./keys");
+
+//create a redis client
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+
+const sub = redisClient.duplicate();
+//what is duplicate? it is a function that creates a new client that will be used for subscibing to channels
+
+function fibonacci(index) {
+  if (index < 2) return 1;
+  return fibonacci(index - 1) + fibonacci(index - 2);
+}
+
+sub.on("message", (channel, indexMsg) => {
+  //what is hsset? it is a function that sets a value in a hash
+  redisClient.hset("values", indexMsg, fibonacci(parseInt(indexMsg)));
+});
+
+//what is subscribe(insert)? it is a function that subscribes to the insert channel
+sub.subscribe("insert"); //subscribe to any insert event (of data in redis)
